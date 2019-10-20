eaa181746279c384c108141b5b7035edfa1776e5
SungMinCho <tjdals4565@gmail.com>
2019-10-20 20:59:16 +0900

    It's a comma

--- imdb_scraper/imdb_scraper/extract_urls.py
+++ imdb_scraper/imdb_scraper/extract_urls.py
@@ -7,5 +7,5 @@ df['url'] = df.imdbId.map(lambda s: 'https://www.imdb.com/title/tt0' + str(s))
 with open('urllist.py', 'w') as f:
   f.write('URLLIST = [\n')
   for url in df['url']:
-    f.write("  '" + url + "'.\n")
+    f.write("  '" + url + "',\n")
   f.write("]\n")
